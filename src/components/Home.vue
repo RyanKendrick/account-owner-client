4da9d500373f67187607979b66cabd1d6b6b01f1
RyanKendrick <rpkendrick@gmail.com>
2020-03-17 21:08:48 -0400

    progress

---
+++ src/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+        <p class="homeText">
+            ACCOUNT OWNER VUE.JS APP
+        </p>
+</template>
+<script>
+export default {
+    name: 'Home'
+};
+</script>>
+<style scoped>
+.homeText {
+    font-size: 35px;
+    color: blue;
+    text-align: center;
+    position: relative;
+    top: 30px;
+    text-shadow: 2px 2px 2px gray;
+}
+</style>
